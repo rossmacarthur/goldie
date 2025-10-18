78909e8e2a614a60b426acfa2cbef105b7892727
Ross MacArthur <ross@macarthur.io>
2025-10-18 14:50:13 +0200

    Add builder API for customizing golden file
    - Add a new `Builder` struct for configuring the location of the golden
      file. You can now specify the golden file directory, override the
      name, name prefix, name suffix, and extension of the golden file.
    - Change how the default golden path is computed to better handle nested
      modules. The new logic will include any submodules in the test module
      within the golden file path.

--- src/lib.rs
+++ src/lib.rs
@@ -50,24 +50,19 @@
 #[cfg(test)]
 mod tests;
 
-use std::collections::BTreeMap;
 use std::env;
-use std::ffi::OsStr;
 use std::fs;
-use std::path::{Path, PathBuf};
-use std::process;
-use std::sync::Arc;
-use std::sync::Mutex;
+use std::path::{Component, Path, PathBuf};
 
 use anyhow::{Context, Result};
 use once_cell::sync::Lazy;
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 
 /// Assert the golden file matches.
 #[macro_export]
 macro_rules! assert {
     ($actual:expr) => {{
-        let g = $crate::_new_goldie!();
+        let g = $crate::builder!().build();
         if let Err(err) = g.assert($actual) {
             ::std::panic!("{}", err);
         }
@@ -78,7 +73,7 @@ macro_rules! assert {
 #[macro_export]
 macro_rules! assert_debug {
     ($actual:expr) => {{
-        let g = $crate::_new_goldie!();
+        let g = $crate::builder!().build();
         if let Err(err) = g.assert_debug($actual) {
             ::std::panic!("{}", err);
         }
@@ -89,7 +84,7 @@ macro_rules! assert_debug {
 #[macro_export]
 macro_rules! assert_template {
     ($ctx:expr, $actual:expr) => {{
-        let g = $crate::_new_goldie!();
+        let g = $crate::builder!().build();
         if let Err(err) = g.assert_template($ctx, $actual) {
             ::std::panic!("{}", err);
         }
@@ -100,7 +95,7 @@ macro_rules! assert_template {
 #[macro_export]
 macro_rules! assert_json {
     ($actual:expr) => {{
-        let g = $crate::_new_goldie!();
+        let g = $crate::builder!().build();
         if let Err(err) = g.assert_json($actual) {
             ::std::panic!("{}", err);
         }
@@ -108,15 +103,14 @@ macro_rules! assert_json {
 }
 
 /// Constructs a new goldie instance.
-///
-/// Not public API.
 #[doc(hidden)]
 #[macro_export]
-macro_rules! _new_goldie {
+macro_rules! builder {
     () => {{
-        let source_file = $crate::cargo_workspace_dir(env!("CARGO_MANIFEST_DIR")).join(file!());
-        let function_path = $crate::_function_path!();
-        $crate::Goldie::new(source_file, function_path)
+        let source_manifest_dir = ::std::env!("CARGO_MANIFEST_DIR");
+        let source_file = ::std::file!();
+        let function_path = $crate::function_path!();
+        $crate::Builder::new(source_manifest_dir, source_file, function_path)
     }};
 }
 
@@ -127,7 +121,7 @@ macro_rules! _new_goldie {
 /// Not public API.
 #[doc(hidden)]
 #[macro_export]
-macro_rules! _function_path {
+macro_rules! function_path {
     () => {{
         fn f() {}
         fn type_name_of_val<T>(_: T) -> &'static str {
@@ -141,6 +135,37 @@ macro_rules! _function_path {
     }};
 }
 
+/// Builder for configuring a goldie assertion.
+#[derive(Debug, Clone)]
+pub struct Builder {
+    /// env!("CARGO_MANIFEST_DIR") in the test
+    source_manifest_dir: PathBuf,
+
+    /// file!() in the test, e.g. src/module/tests/file.rs
+    source_file: PathBuf,
+
+    /// _function_path!() in the test, e.g. crate::module::tests::function_name
+    function_path: PathBuf,
+
+    /// Override the directory we should put the golden file in
+    golden_dir: Option<PathBuf>,
+
+    /// Override the name of the assertion, this is usually derived from the test function name
+    name: Option<String>,
+
+    /// Add a prefix to the name
+    name_prefix: Option<String>,
+
+    /// Add a suffix to the name
+    name_suffix: Option<String>,
+
+    /// Override the extension of the golden file
+    extension: Option<String>,
+
+    /// Whether to update the golden file if it doesn't match
+    update: Option<bool>,
+}
+
 #[derive(Debug)]
 pub struct Goldie {
     /// The path to the golden file.
@@ -149,39 +174,180 @@ pub struct Goldie {
     update: bool,
 }
 
-impl Goldie {
-    /// Construct a new golden file tester.
-    ///
-    /// Where
-    /// - `source_file` is path to the source file that the test resides in.
-    /// - `function_path` is the full path to the function. e.g.
-    ///   `crate::module::tests::function_name`.
-    pub fn new(source_file: impl AsRef<Path>, function_path: impl AsRef<str>) -> Self {
-        Self::new_impl(source_file.as_ref(), function_path.as_ref())
+impl Builder {
+    #[doc(hidden)]
+    pub fn new(
+        source_manifest_dir: &'static str,
+        source_file: &'static str,
+        function_path: &'static str,
+    ) -> Self {
+        Self {
+            source_manifest_dir: PathBuf::from(source_manifest_dir),
+            source_file: PathBuf::from(source_file),
+            function_path: function_path.split("::").collect(),
+            name: None,
+            name_prefix: None,
+            name_suffix: None,
+            extension: None,
+            golden_dir: None,
+            update: None,
+        }
     }
 
-    fn new_impl(source_file: &Path, function_path: &str) -> Self {
-        let (_, name) = function_path.rsplit_once("::").unwrap();
+    /// Override the golden file directory
+    ///
+    /// Defaults to a "testdata" directory next to the source test module.
+    pub fn golden_dir(mut self, dir: impl Into<PathBuf>) -> Self {
+        self.golden_dir = Some(dir.into());
+        self
+    }
+
+    /// Override the name of the assertion
+    ///
+    /// This defaults to the name of the test function.
+    pub fn name(mut self, name: impl Into<String>) -> Self {
+        self.name = Some(name.into());
+        self
+    }
+
+    /// Add a suffix to the name of the assertion
+    ///
+    /// This is useful for parameterized tests. Defaults to no prefix.
+    ///
+    /// It is allowed to contain a path separator.
+    pub fn name_prefix(mut self, prefix: impl Into<String>) -> Self {
+        self.name_prefix = Some(prefix.into());
+        self
+    }
+
+    /// Add a suffix to the name of the assertion
+    ///
+    /// This is useful for parameterized tests. Defaults to no suffix.
+    ///
+    /// It is allowed to contain a path separator.
+    pub fn name_suffix(mut self, suffix: impl Into<String>) -> Self {
+        self.name_suffix = Some(suffix.into());
+        self
+    }
+
+    /// Override the extension of the golden file
+    ///
+    /// Defaults to "golden".
+    pub fn extension(mut self, extension: impl Into<String>) -> Self {
+        self.extension = Some(extension.into());
+        self
+    }
+
+    /// Override whether to update the golden file if it doesn't match
+    ///
+    /// Defaults to the value of the `GOLDIE_UPDATE` environment variable.
+    pub fn update(mut self, update: bool) -> Self {
+        self.update = Some(update);
+        self
+    }
+
+    pub fn build(self) -> Goldie {
+        let Self {
+            source_manifest_dir,
+            source_file,
+            function_path,
+            name,
+            name_prefix,
+            name_suffix,
+            extension,
+            golden_dir,
+            update,
+        } = self;
+
+        // first figure out the name of the test
+        let mut name = name.unwrap_or_else(|| {
+            function_path
+                .file_name()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_owned()
+        });
+        if let Some(prefix) = &name_prefix {
+            name = format!("{prefix}{name}");
+        }
+        if let Some(suffix) = &name_suffix {
+            name = format!("{name}{suffix}");
+        }
+
+        let skip_mod = |c: &Component| c.as_os_str() != "lib" && c.as_os_str() != "mod";
+        let skip_tests = |c: &Component| c.as_os_str() != "tests";
+
+        // Convert a source file path like "src/a/mod.rs" into "src/a"
+        let source_file: PathBuf = source_file
+            .with_extension("")
+            .components()
+            .filter(skip_mod)
+            .collect();
+
+        // Remove the leading crate name from the function path
+        let function_path: PathBuf = function_path.components().skip(1).collect();
+
+        // Find the longest prefix of the function path that is a suffix of the source file
+        // E.g. src/a/b, a/b/tests/c/func will find
+        //      src, a/b, tests/c/func
+        let (source_file, remaining_mods, function_path) =
+            match_paths(&source_file, &function_path);
+
+        let golden_dir = golden_dir.unwrap_or_else(|| {
+            let mut d = source_manifest_dir.clone();
+            d.push(source_file);
+            d.extend(remaining_mods.components().filter(skip_tests));
+            d.push("testdata");
+            d
+        });
 
         let golden_file = {
-            let mut p = source_file.parent().unwrap().to_owned();
-            p.push("testdata");
-            p.push(name);
-            p.set_extension("golden");
-            p
+            let fpath: PathBuf = function_path.components().filter(skip_tests).collect();
+            let mut f = golden_dir;
+            f.push(fpath.parent().unwrap_or(Path::new("")));
+            f.push(name);
+            f.set_extension(extension.as_deref().unwrap_or("golden"));
+            f
         };
 
-        let update = matches!(
-            env::var("GOLDIE_UPDATE").ok().as_deref(),
-            Some("1" | "true")
-        );
+        let update = update.unwrap_or_else(|| {
+            matches!(
+                env::var("GOLDIE_UPDATE").ok().as_deref(),
+                Some("1" | "true")
+            )
+        });
 
-        Self {
+        Goldie {
             golden_file,
             update,
         }
     }
+}
 
+fn match_paths(left: &Path, right: &Path) -> (PathBuf, PathBuf, PathBuf) {
+    let lc = Vec::from_iter(left.components());
+    let rc = Vec::from_iter(right.components());
+
+    // Find the max suffix of lc that is a prefix of rc
+    let j = (0..=rc.len())
+        .rev()
+        .find(|&j| {
+            let i = lc.len().saturating_sub(j);
+            lc[i..] == rc[..j]
+        })
+        .unwrap_or(0);
+
+    // Split by that suffix
+    let i = lc.len().saturating_sub(j);
+    let left = lc[..i].iter().collect();
+    let common = lc[i..].iter().collect();
+    let right = rc[j..].iter().collect();
+
+    (left, common, right)
+}
+
+impl Goldie {
     #[track_caller]
     pub fn assert(&self, actual: impl AsRef<str>) -> Result<()> {
         if self.update {
@@ -273,43 +439,4 @@ impl Goldie {
             "GOLDIE_UPDATE=1".blue().bold(),
         )
     }
-}
-
-/// Returns the Cargo workspace dir for the given manifest dir.
-///
-/// Not public API.
-#[doc(hidden)]
-pub fn cargo_workspace_dir(manifest_dir: &str) -> PathBuf {
-    static DIRS: Lazy<Mutex<BTreeMap<String, Arc<Path>>>> =
-        Lazy::new(|| Mutex::new(BTreeMap::new()));
-
-    let mut dirs = DIRS.lock().unwrap();
-
-    if let Some(dir) = dirs.get(manifest_dir) {
-        return dir.to_path_buf();
-    }
-
-    let dir = env::var("CARGO_WORKSPACE_DIR")
-        .map(PathBuf::from)
-        .unwrap_or_else(|_| {
-            #[derive(Deserialize)]
-            struct Manifest {
-                workspace_root: PathBuf,
-            }
-            let cargo = env::var_os("CARGO");
-            let cargo = cargo.as_deref().unwrap_or_else(|| OsStr::new("cargo"));
-            let output = process::Command::new(cargo)
-                .args(["metadata", "--format-version=1", "--no-deps"])
-                .current_dir(manifest_dir)
-                .output()
-                .unwrap();
-            let manifest: Manifest = serde_json::from_slice(&output.stdout).unwrap();
-            manifest.workspace_root
-        });
-    dirs.insert(
-        String::from(manifest_dir),
-        dir.clone().into_boxed_path().into(),
-    );
-
-    dir
 }
--- src/tests.rs
+++ src/tests.rs
@@ -4,32 +4,97 @@ use serde::Serialize;
 
 #[test]
 fn goldie_golden_file() {
+    let manifest_dir = "/repo";
     let tests = [
         (
-            ("/repo/src/lib.rs", "crate::tests::func"),
+            ("src/lib.rs", "crate::tests::func"),
             "/repo/src/testdata/func.golden",
         ),
         (
-            ("/repo/src/foo.rs", "crate::foo::tests::func"),
+            ("src/lib.rs", "crate::tests::a::func"),
+            "/repo/src/testdata/a/func.golden",
+        ),
+        (
+            ("src/tests.rs", "crate::tests::func"),
             "/repo/src/testdata/func.golden",
         ),
         (
-            ("/repo/src/foo/mod.rs", "crate::foo::tests::func"),
-            "/repo/src/foo/testdata/func.golden",
+            ("src/tests.rs", "crate::tests::a::func"),
+            "/repo/src/testdata/a/func.golden",
         ),
         (
-            ("/repo/src/foo/bar.rs", "crate::foo::bar::tests::func"),
-            "/repo/src/foo/testdata/func.golden",
+            ("src/a.rs", "crate::a::tests::func"),
+            "/repo/src/a/testdata/func.golden",
         ),
         (
-            ("/repo/src/bin/foo.rs", "crate::tests::func"),
-            "/repo/src/bin/testdata/func.golden",
+            ("src/a.rs", "crate::a::tests::b::func"),
+            "/repo/src/a/testdata/b/func.golden",
+        ),
+        (
+            ("src/a/tests.rs", "crate::a::tests::func"),
+            "/repo/src/a/testdata/func.golden",
+        ),
+        (
+            ("src/a/tests.rs", "crate::a::tests::b::func"),
+            "/repo/src/a/testdata/b/func.golden",
+        ),
+        (
+            ("src/a/mod.rs", "crate::a::tests::func"),
+            "/repo/src/a/testdata/func.golden",
+        ),
+        (
+            ("src/a/mod.rs", "crate::a::tests::b::func"),
+            "/repo/src/a/testdata/b/func.golden",
+        ),
+        (
+            ("src/a/tests.rs", "crate::a::tests::func"),
+            "/repo/src/a/testdata/func.golden",
+        ),
+        (
+            ("src/a/tests.rs", "crate::a::tests::b::func"),
+            "/repo/src/a/testdata/b/func.golden",
+        ),
+        (
+            ("src/a/b/tests.rs", "crate::a::b::tests::func"),
+            "/repo/src/a/b/testdata/func.golden",
+        ),
+        (
+            ("src/a/b/tests.rs", "crate::a::b::tests::c::func"),
+            "/repo/src/a/b/testdata/c/func.golden",
+        ),
+        (
+            ("src/bin/a.rs", "crate::tests::func"),
+            "/repo/src/bin/a/testdata/func.golden",
+        ),
+        (
+            ("src/bin/a.rs", "crate::tests::b::func"),
+            "/repo/src/bin/a/testdata/b/func.golden",
+        ),
+        (
+            ("tests/a.rs", "crate::func"),
+            "/repo/tests/a/testdata/func.golden",
+        ),
+        (
+            ("tests/a.rs", "crate::b::func"),
+            "/repo/tests/a/testdata/b/func.golden",
+        ),
+        (
+            ("tests/a/b.rs", "crate::b::func"),
+            "/repo/tests/a/b/testdata/func.golden",
+        ),
+        (
+            ("tests/a/b.rs", "crate::b::c::func"),
+            "/repo/tests/a/b/testdata/c/func.golden",
         ),
     ];
 
-    for ((file, path), exp) in tests {
-        let g = Goldie::new(file, path);
-        assert_eq!(g.golden_file, Path::new(exp))
+    for ((source_file, function_path), exp) in tests {
+        let g = Builder::new(manifest_dir, source_file, function_path).build();
+        assert_eq!(
+            g.golden_file,
+            Path::new(exp),
+            "source_file: {source_file}, function_path: {function_path}",
+        );
     }
 }
 
